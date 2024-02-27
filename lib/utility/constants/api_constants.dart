ec4e9d86e52181d5bfd2fe8e59be1fb07423e729
Andra2809 <andra.stoica123@gmail.com>
2024-02-27 20:03:25 +0200

    Change for last commit

--- lib/utility/constants/api_constants.dart
+++ lib/utility/constants/api_constants.dart
@@ -5,21 +5,5 @@ class ApiConstants {
   static const String register = "$baseUser/Register";
   static const String login = "$baseUser/Login";
   static const String getProfile = "$baseUser/GetUserProfile/";
-  static const String changePassword = "$baseUser/ChangePassword";
-  static const String updateProfile = "$baseUser/UpdateProfile";
 
-  static const String baseTicket = "$baseUrl/api/TicketBooking/GetAllTickets";
-  static const String baseTicketBooking = "$baseUrl/api/TicketBooking";
-  static const String addTicketBooking = "$baseTicketBooking/AddTicketBooking";
-  static const String getTicketBookingByUserId =
-      "$baseTicketBooking/GetUserTicketBooking/";
-  static const String getTicketBookingById =
-      "$baseTicketBooking/GetTicketBookingById/";
-
-  static const String baseDirection = "$baseUrl/api/SavedDirection";
-  static const String addDirection = "$baseDirection/AddSavedDirection";
-  static const String updateDirection = "$baseDirection/UpdateSavedDirection";
-  static const String deleteDirection = "$baseDirection/DeleteSavedDirection/";
-  static const String getDirectionByUserId =
-      "$baseDirection/GetUserSavedDirection/";
 }
