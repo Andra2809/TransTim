0188cf501a7bf8c1b5b4ca90cc628da8555738db
Andra2809 <andra.stoica123@gmail.com>
2024-03-26 00:56:42 +0200

    added backend functionality for saved direction details screen

---
+++ backend/webapi_demo/Controllers/SavedDirectionController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Web.Http; 
+using static webapi_demo.Models.SavedDirections;
+
+namespace webapi_demo.Controllers
+{
+    public class SavedDirectionController : ApiController
+    {
+        Models.SavedDirections savedDirection = new Models.SavedDirections();
+
+        [HttpGet]
+        public string getanydata()
+        {
+            return savedDirection.InvalidRequest();
+        } 
+
+
+        [HttpPost]
+        [Route("api/SavedDirection/AddSavedDirection")]
+        public HttpResponseMessage AddSavedDirection([FromBody] SavedDirectionsModel savedDirectionModel)
+        {
+            return Models.Helper.getResponse(savedDirection.AddSavedDirections(savedDirectionModel));
+        }
+
+
+        [HttpPost]
+        [Route("api/SavedDirection/UpdateSavedDirection")]
+        public HttpResponseMessage UpdateSavedDirection([FromBody] SavedDirectionsModel savedDirectionModel)
+        {
+            return Models.Helper.getResponse(savedDirection.UpdateSavedDirections(savedDirectionModel));
+        }
+
+
+
+        [HttpGet]
+        [Route("api/SavedDirection/GetUserSavedDirection/{userId}")]
+        public HttpResponseMessage GetUserSavedDirection(string userId)
+        {
+            return Models.Helper.getResponse(savedDirection.GetSavedDirectionsByUserId(userId));
+        }
+
+        [HttpGet]
+        [Route("api/SavedDirection/DeleteSavedDirection/{userId}")]
+        public HttpResponseMessage DeleteSavedDirection(string userId)
+        {
+            return Models.Helper.getResponse(savedDirection.DeleteSavedDirections(userId));
+        }
+         
+
+    }
+}
---
+++ backend/webapi_demo/Models/SavedDirections.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web;
+
+namespace webapi_demo.Models
+{
+    public class SavedDirections
+    {
+        JSONMaker jm = new JSONMaker();
+
+        public string InvalidRequest()
+        {
+            return jm.Singlevalue("Invalid Request");
+        }
+
+        public class SavedDirectionsModel
+        {
+            public string directionId { get; set; }
+            public string userId { get; set; }
+            public string startLocation { get; set; }
+            public string endLocation { get; set; }
+            public string startLocationLatLng { get; set; }
+            public string endLocationLatLng { get; set; }
+            public string mode { get; set; }
+            public string travelMode { get; set; }
+            public string dateTime { get; set; }
+
+        }
+
+        public string AddSavedDirections(SavedDirectionsModel savedDirectionsModel)
+        {
+            SqlConnection con = Database.getDB();
+            string ans = "";
+
+            try
+            {
+                string query = "Insert into [SavedDirections] (userId,startLocation,endLocation," +
+                    "startLocationLatLng,endLocationLatLng,mode,travelMode,dateTime) values " +
+                    "(@userId,@startLocation,@endLocation,@startLocationLatLng," +
+                    "@endLocationLatLng,@mode,@travelMode,@dateTime)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@userId", savedDirectionsModel.userId);
+                cmd.Parameters.AddWithValue("@startLocation", savedDirectionsModel.startLocation);
+                cmd.Parameters.AddWithValue("@endLocation", savedDirectionsModel.endLocation);
+                cmd.Parameters.AddWithValue("@startLocationLatLng", savedDirectionsModel.startLocationLatLng);
+                cmd.Parameters.AddWithValue("@endLocationLatLng", savedDirectionsModel.endLocationLatLng);
+                cmd.Parameters.AddWithValue("@mode", savedDirectionsModel.mode);
+                cmd.Parameters.AddWithValue("@travelMode", savedDirectionsModel.travelMode);
+                cmd.Parameters.AddWithValue("@dateTime", savedDirectionsModel.dateTime);
+
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                //true
+                ans = jm.Singlevalue("true");
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                ans = jm.Error(e.Message);
+            }
+
+            return ans;
+        }
+
+        public string UpdateSavedDirections(SavedDirectionsModel savedDirectionsModel)
+        {
+            SqlConnection con = Database.getDB();
+            string ans = "";
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select * from [SavedDirections] " +
+                   "where directionId=@directionId", con);
+                da.SelectCommand.Parameters.AddWithValue("@directionId", savedDirectionsModel.directionId);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                int count = ds.Tables[0].Rows.Count;
+                if (count > 0)
+                {
+                    string query = "Update [SavedDirections] set userId=@userId," +
+                        "startLocation=@startLocation,endLocation=@endLocation," +
+                        "startLocationLatLng=@startLocationLatLng,endLocationLatLng=@endLocationLatLng" +
+                        "mode=@mode,travelMode=@travelMode" +
+                        "dateTime=@dateTime where directionId = @directionId ";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@directionId", savedDirectionsModel.directionId);
+                    cmd.Parameters.AddWithValue("@userId", savedDirectionsModel.userId);
+                    cmd.Parameters.AddWithValue("@startLocation", savedDirectionsModel.startLocation);
+                    cmd.Parameters.AddWithValue("@endLocation", savedDirectionsModel.endLocation);
+                    cmd.Parameters.AddWithValue("@startLocationLatLng", savedDirectionsModel.startLocationLatLng);
+                    cmd.Parameters.AddWithValue("@endLocationLatLng", savedDirectionsModel.endLocationLatLng);
+                    cmd.Parameters.AddWithValue("@mode", savedDirectionsModel.mode);
+                    cmd.Parameters.AddWithValue("@travelMode", savedDirectionsModel.travelMode);
+                    cmd.Parameters.AddWithValue("@dateTime", savedDirectionsModel.dateTime);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    //true
+                    ans = jm.Singlevalue("true");
+                }
+                else
+                {
+                    //true
+                    ans = jm.Singlevalue("no");
+                }
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                ans = jm.Error(e.Message);
+            }
+
+            return ans;
+        }
+
+
+        public string GetSavedDirectionsByUserId(String userId)
+        {
+            SqlConnection con = Database.getDB();
+            string ans = "";
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select * from [SavedDirections] " +
+                    "where SavedDirections.userId = @userId ", con);
+                da.SelectCommand.Parameters.AddWithValue("@userId", userId);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                int count = ds.Tables[0].Rows.Count;
+                if (count > 0)
+                {
+                    //Already Exist
+                    ans = jm.Maker(ds);
+                }
+                else
+                {
+                    ans = jm.Singlevalue("no");
+                }
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                ans = jm.Error(e.Message);
+            }
+
+            return ans;
+        }
+
+        public string DeleteSavedDirections(string directionId)
+        {
+            SqlConnection con = Database.getDB();
+            string ans = "";
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select directionId  from " +
+                    "[SavedDirections] where directionId = @directionId ", con);
+                da.SelectCommand.Parameters.AddWithValue("@directionId ", directionId);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                int count = ds.Tables[0].Rows.Count;
+                if (count == 0)
+                {
+                    //no
+                    ans = jm.Singlevalue("no");
+                }
+                else
+                {
+                    string query = "DELETE FROM [SavedDirections] WHERE directionId  ='" + directionId + "'";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    //true
+                    ans = jm.Singlevalue("true");
+                }
+            }
+            catch (Exception e)
+            {
+                ans = jm.Error(e.Message);
+            }
+            return ans;
+        }
+    }
+}
