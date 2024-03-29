97c725e56dcf0be739941f70dce98409900d9249
Andra2809 <andra.stoica123@gmail.com>
2024-03-29 22:27:42 +0200

    backend implementation for ticket booking

---
+++ backend/webapi_demo/Controllers/TicketBookingController.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using static webapi_demo.Models.TicketBooking;
+
+namespace webapi_demo.Controllers
+{
+    public class TicketBookingController : ApiController
+    {
+        Models.TicketBooking ticketBooking = new Models.TicketBooking();
+
+        [HttpGet]
+        public string getanydata()
+        {
+            return ticketBooking.InvalidRequest();
+        }
+
+        [HttpGet]
+        [Route("api/TicketBooking/GetAllTickets/")]
+        public HttpResponseMessage GetAllTickets()
+        {
+            return Models.Helper.getResponse(ticketBooking.GetAllTickets());
+        }
+
+        [HttpPost]
+        [Route("api/TicketBooking/AddTicketBooking")]
+        public HttpResponseMessage AddTicketBooking([FromBody] TicketBookingModel ticketBookingModel)
+        {
+            return Models.Helper.getResponse(ticketBooking.AddTicketBooking(ticketBookingModel));
+        } 
+
+        [HttpGet]
+        [Route("api/TicketBooking/GetUserTicketBooking/{userId}")]
+        public HttpResponseMessage GetUserTicketBooking(string userId)
+        {
+            return Models.Helper.getResponse(ticketBooking.GetTicketBookingByUserId(userId));
+        }
+
+        [HttpGet]
+        [Route("api/TicketBooking/GetTicketBookingById/{ticketBookingId}")]
+        public HttpResponseMessage GetTicketBookingById(string ticketBookingId)
+        {
+            return Models.Helper.getResponse(ticketBooking.GetTicketBookingByTicketId(ticketBookingId));
+        }
+
+    }
+}
---
+++ backend/webapi_demo/Models/TicketBooking.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Transactions;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace webapi_demo.Models
+{
+    public class TicketBooking
+    {
+        JSONMaker jm = new JSONMaker();
+
+        public string InvalidRequest()
+        {
+            return jm.Singlevalue("Invalid Request");
+        }
+
+        public class TicketBookingModel
+        {
+            public string ticketBookingId { get; set; }
+            public string ticketId { get; set; }
+            public string userId { get; set; }
+            public string date { get; set; }
+            public string time { get; set; }
+            public string passengerCount { get; set; }
+            public string totalAmount { get; set; }
+            public string description { get; set; }
+        }
+
+        public string GetAllTickets()
+        {
+            SqlConnection con = Database.getDB();
+            string ans = "";
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select * from [Ticket]", con);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                int count = ds.Tables[0].Rows.Count;
+                if (count > 0)
+                {
+                    //Already Exist
+                    ans = jm.Maker(ds);
+                }
+                else
+                {
+                    ans = jm.Singlevalue("no");
+                }
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                ans = jm.Error(e.Message);
+            }
+
+            return ans;
+        }
+
+        public string AddTicketBooking(TicketBookingModel ticketBookingsModel)
+        {
+            string ans = "";
+
+            using (SqlConnection con = Database.getDB())
+            {
+                try
+                {
+                    string query = "Insert into [TicketBooking] (ticketId,userId, date, time, passengerCount, totalAmount) " +
+                        "values (@ticketId,@userId, @date, @time, @passengerCount, @totalAmount);SELECT SCOPE_IDENTITY();";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.Add("@ticketId", SqlDbType.VarChar).Value = ticketBookingsModel.ticketId;
+                        cmd.Parameters.Add("@userId", SqlDbType.VarChar).Value = ticketBookingsModel.userId;
+                        cmd.Parameters.Add("@date", SqlDbType.VarChar).Value = ticketBookingsModel.date;
+                        cmd.Parameters.Add("@time", SqlDbType.VarChar).Value = ticketBookingsModel.time;
+                        cmd.Parameters.Add("@passengerCount", SqlDbType.VarChar).Value = ticketBookingsModel.passengerCount;
+                        cmd.Parameters.Add("@totalAmount", SqlDbType.VarChar).Value = ticketBookingsModel.totalAmount;
+
+                        con.Open();
+                        int ticketBookingId = Convert.ToInt32(cmd.ExecuteScalar());
+                        con.Close();
+
+                        ans = jm.Singlevalue("true");
+                    }
+                }
+                catch (Exception e)
+                {
+                    ans = jm.Error(e.Message);
+                }
+            }
+
+            return ans;
+        }
+
+    }
+}
